2ac385c8b9dbccddd93e6032e26b5ae3de90de7a
sumit10git <144229107+sumit10git@users.noreply.github.com>
2023-10-15 18:23:18 +0530

    Add files via upload

---
+++ Project-2(Guess the Number Game)/Project1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{
+    int number, guess, nguesses = 0;
+    srand(time(0));
+    number = rand() % 100 + 1; // it will generate the random between 1 to 100
+        //printf("the number is %d\n", number);
+    // keep running the looop until the number is guessed
+    do
+    {
+        printf("guess the number between 1 to 10\n");
+        scanf("%d", &guess);
+        if (guess > number)
+        {
+            printf("lower number please!\n");
+        }
+        else if (guess < number)
+        {
+            printf("higher number please!\n");
+        }
+        else
+        {
+            printf("you guessed it in %d attempts\n");
+        }
+        nguesses++;
+
+    } while (guess != number);
+    return 0;
+}
