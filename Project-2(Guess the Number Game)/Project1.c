f7445305e3d680055f5aaa6a71904bc153a0982b
sumit10git <144229107+sumit10git@users.noreply.github.com>
2023-10-27 16:22:13 +0530

    Update Project1.c

--- Project-2(Guess the Number Game)/Project1.c
+++ Project-2(Guess the Number Game)/Project1.c
@@ -6,7 +6,7 @@ int main()
 {
     int number, guess, nguesses = 0;
     srand(time(0));
-    number = rand() % 100 + 1; // it will generate the random between 1 to 100
+    number = rand() % 10 + 1; // it will generate the random between 1 to 10
         //printf("the number is %d\n", number);
     // keep running the looop until the number is guessed
     do
